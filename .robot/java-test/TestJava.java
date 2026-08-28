be593f272d2c350756a29c66df243e71d6b23d3a
catatanbahrul-arch <catatanbahrul-arch@users.noreply.github.com>
2026-08-28 14:05:48 +0700

    robot: record failed command

--- .robot/java-test/TestJava.java
+++ .robot/java-test/TestJava.java
@@ -1,7 +1,8 @@
 public class TestJava {
     public static void main(String[] args) {
-        System.out.println("JAVA_PROCESS_OK");
-        System.out.println("Processors=" + Runtime.getRuntime().availableProcessors());
-        System.out.println("MaxMemory=" + Runtime.getRuntime().maxMemory());
+        System.out.println("JAVA_OK");
+        System.out.println("PID=" + ProcessHandle.current().pid());
+        System.out.println("PROCESSORS=" + Runtime.getRuntime().availableProcessors());
+        System.out.println("MAX_MEMORY=" + Runtime.getRuntime().maxMemory());
     }
 }
